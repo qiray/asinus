d7c5054e9ec31064f6c1de8487256a941ecf3968
qiray <zotovyaa@mail.ru>
2018-08-24 18:06:59 +0300

    some extra functions

--- js/index.js
+++ js/index.js
@@ -12,8 +12,6 @@ function showWeightsTable() {
 
 function weightTabledeleteRow(index) {
     let obj = document.getElementById("weightTableRow" + index);
-    console.log(index);
-    console.log(obj);
     if (obj === undefined)
         return;
     obj.remove();
@@ -34,11 +32,47 @@ function weightTableAddRow() {
         <div class="divTableCellSmall"><button class="plusButton" id="deleteCriterionButton` + index + `">-</button></div>
     </div>`; //TODO: replace with addElement
     appData.addCriterion();
-    table.innerHTML += row_template;
-    // document.getElementById('deleteCriterionButton' + index).onclick = function() {
-    //     console.log(index);
-    //     weightTabledeleteRow(index);
-    // };
+    table.appendChild(newWeightTableRowElements(index));
+    document.getElementById('deleteCriterionButton' + index).onclick = function() {
+        weightTabledeleteRow(index);
+    };
+}
+
+function tableNewColumn(type, columnClassName, objClassName, params) { //TODO: test
+    var column = document.createElement('div');
+    column.className = columnClassName;
+    var obj = document.createElement('type');
+    obj.className = objClassName;
+    for (let prop in params) {
+        obj[prop] = params[prop];
+    }
+    column.appendChild(obj);
+    return column;
+}
+
+function newWeightTableRowElements(index) { //TODO: test
+    var container = document.createElement('div');
+    container.className = "divTableRow";
+    container.id = "weightTableRow" + weightTableRow;
+
+    var column1 = document.createElement('div');
+    column1.className = "divTableCell";
+    var input1 = document.createElement('input');
+    input1.className = "tableInput";
+    input1.type = "text";
+    column1.appendChild(input1);
+    container.appendChild(column1);
+
+    var column2 = document.createElement('div');
+    column1.className = "divTableCell";
+    var input2 = document.createElement('input');
+    input2.className = "tableInput";
+    input2.type = "number";
+    input2.min = "0";
+    column2.appendChild(input2);
+    container.appendChild(column2);
+
+    return container;
 }
 
 document.getElementById('editWeights').onclick = function() {
@@ -48,7 +82,3 @@ document.getElementById('editWeights').onclick = function() {
 document.getElementById('weightsPlusButton').onclick = function() {
     weightTableAddRow();
 };
-
-function a() {
-    console.log(11);
-}
